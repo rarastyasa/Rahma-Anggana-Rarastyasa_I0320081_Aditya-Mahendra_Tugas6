2c5324cebe1577d684d3f540a0cfead3f676a3ee
Rahma Anggana Rarastyasa <rahmaanggana04@gmail.com>
2021-04-10 01:34:58 +0700

    Praktikum Programa Komputer Tugas 6

---
+++ I0320081_Exercise 6.3.py
@@ -0,0 +1,6 @@
+#melakukan pengulangan
+i = 1
+while i <= 10:
+    print("Baris ke-%d: Hello world" % i )
+    i = i + 1
+    
---
+++ I0320081_Soal1_Tugas6.py
@@ -0,0 +1,5 @@
+print("***Mencetak Kata Sebanyak 10 Kali***")
+i = 1
+while i <= 10:
+    print("Baris ke-%d: Hello World!"  % i)
+    i = i + 1
